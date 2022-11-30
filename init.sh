6793b88fb66479284634bc75c5bc67dbe9fe760c
Thibaut SIGUENZA <tsiguenz@e1r1p6.clusters.42paris.fr>
2022-11-30 09:32:29 +0100

    fix while functions dir is not created

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ vim_folder_path=~/.vim/
 vimrc_path=~/.vimrc
 
 echo "Copy directory functions/ in $vim_folder_path"
+mkdir ~/.vim/functions
 cp -iurf functions/ $vim_folder_path
 
 echo "Copy vimrc in $vimrc_path"
