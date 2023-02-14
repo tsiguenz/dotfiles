134954f2df17840e8e5f58de27b2c3b41d699a7a
Thibaut SIGUENZA <tsiguenz@e2r1p1.clusters.42paris.fr>
2023-02-14 17:42:47 +0100

    add neovim config for web project

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 vim_folder_path=~/.vim/
 vimrc_path=~/.vimrc
 tmux_conf_path=~/.tmux.conf
+nvim_folder_path=~/.config/nvim/
 
 echo "Copy directory functions/ in $vim_folder_path"
 mkdir -p ~/.vim/functions
@@ -13,3 +14,7 @@ cp -iuf vimrc $vimrc_path
 
 echo "Copy tmux.conf in $tmux_conf_path"
 cp -iuf tmux.conf $tmux_conf_path
+
+echo "Copy nvim config to $nvim_folder_path"
+mkdir -p $nvim_folder_path
+cp -iurf nvim_config $nvim_folder_path/init.vim
