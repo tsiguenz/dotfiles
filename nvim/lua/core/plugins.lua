8b23ad827f0c496d183e15dc1198f5f90db100b7
tsiguenz <tsiguenz@student.42.fr>
2023-07-20 01:00:24 +0200

    add lsp support

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -1,37 +1,62 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  -- Theme
-  use 'ellisonleao/gruvbox.nvim'
-  use 'lukas-reineke/indent-blankline.nvim'
-  -- Code parser
-  use 'nvim-treesitter/nvim-treesitter'
-  -- Powerline for nvim
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
-  -- File explorer and so forth
-  use {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.2',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+	use 'wbthomason/packer.nvim'
+	-- Theme
+	use 'ellisonleao/gruvbox.nvim'
+	use 'lukas-reineke/indent-blankline.nvim'
+	-- Code parser
+	use 'nvim-treesitter/nvim-treesitter'
+	-- Powerline for nvim
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+	}
+	-- File explorer and so forth
+	use {
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.2',
+		requires = { 'nvim-lua/plenary.nvim' }
+	}
+	-- LSP from https://miikanissi.com/blog/how-to-setup-nvim-lsp-for-code-analysis-autocompletion-and-linting/
+	use({
+		"neovim/nvim-lspconfig",
+		requires = {
+			-- Automatically install LSPs to stdpath for neovim
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+	})
+	-- Format and lint
+	use("jose-elias-alvarez/null-ls.nvim")
+	-- Autocompletion plugin
+	use { 'hrsh7th/nvim-cmp',
+		requires = {
+			-- Snippet Engine & its associated nvim-cmp source
+			'L3MON4D3/LuaSnip',
+			'saadparwaiz1/cmp_luasnip',
+
+			-- Adds LSP completion capabilities
+			'hrsh7th/cmp-nvim-lsp',
+
+			-- Adds a number of user-friendly snippets
+			'rafamadriz/friendly-snippets',
+		}
+	}
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
