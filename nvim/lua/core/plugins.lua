a906f4044c8ad0adbf935066287a402ca6864ff6
Siguenza92 <thibaut.siguenza@gmail.com>
2023-07-26 03:44:28 +0200

    fix copilot problems on mac

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -1,9 +1,9 @@
 local ensure_packer = function()
 	local fn = vim.fn
-	local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-		vim.cmd [[packadd packer.nvim]]
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
 		return true
 	end
 	return false
@@ -11,33 +11,33 @@ end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
 	-- Theme
-	use 'ellisonleao/gruvbox.nvim'
-	use 'lukas-reineke/indent-blankline.nvim'
+	use("ellisonleao/gruvbox.nvim")
+	use("lukas-reineke/indent-blankline.nvim")
 	-- Code parser
-	use 'nvim-treesitter/nvim-treesitter'
-	use {
-	  "zbirenbaum/copilot.lua",
- 	  cmd = "Copilot",
- 	  event = "InsertEnter",
- 	  config = function()
- 	    require("copilot").setup({
+	use("nvim-treesitter/nvim-treesitter")
+	use({
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
 				suggestion = {
-					auto_trigger = true
-				}
+					accept = false,
+				},
 			})
- 	  end,
-	}
+		end,
+	})
 	-- Powerline for nvim
-	use 'nvim-lualine/lualine.nvim'
+	use("nvim-lualine/lualine.nvim")
 	-- File explorer and so forth
-	use {
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.2',
-		requires = { 'nvim-lua/plenary.nvim' }
-	}
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.2",
+		requires = { "nvim-lua/plenary.nvim" },
+	})
 	-- LSP from https://miikanissi.com/blog/how-to-setup-nvim-lsp-for-code-analysis-autocompletion-and-linting/
 	use({
 		"neovim/nvim-lspconfig",
@@ -48,18 +48,19 @@ return require('packer').startup(function(use)
 		},
 	})
 	-- lint and format
-	use "jose-elias-alvarez/null-ls.nvim"
-	use "jay-babu/mason-null-ls.nvim"
-		-- Autocompletion plugin
-	use { 'hrsh7th/nvim-cmp',
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("jay-babu/mason-null-ls.nvim")
+	-- Autocompletion plugin
+	use({
+		"hrsh7th/nvim-cmp",
 		requires = {
 			-- Adds LSP completion capabilities
-			'hrsh7th/cmp-nvim-lsp',
-		}
-	}
+			"hrsh7th/cmp-nvim-lsp",
+		},
+	})
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
-		require('packer').sync()
+		require("packer").sync()
 	end
 end)
