6e92fb09d10c37f807f2bfaca9a931e98c948613
tsiguenz <tsiguenz@student.42.fr>
2023-07-27 16:53:24 +0200

    add basic config for gdb

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 vim_folder_path=~/.vim/
 vimrc_path=~/.vimrc
 tmux_conf_path=~/.tmux.conf
-nvim_folder_path=~/.config/
+config_folder_path=~/.config/
 
 echo "Copy directory functions/ in $vim_folder_path"
 mkdir -p ~/.vim/functions
@@ -15,6 +15,10 @@ cp -iuf vimrc $vimrc_path
 echo "Copy tmux.conf in $tmux_conf_path"
 cp -iuf tmux.conf $tmux_conf_path
 
-echo "Copy nvim config to $nvim_folder_path"
-mkdir -p $nvim_folder_path
-cp -iurf nvim $nvim_folder_path
+echo "Copy nvim config to $config_folder_path"
+mkdir -p $config_folder_path
+cp -iurf nvim $config_folder_path
+
+echo "Copy gdb config to $config_folder_path"
+mkdir -p $config_folder_path/gdb
+cp -iurf gdbinit $config_folder_path/gdb/
