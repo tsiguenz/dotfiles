8cb5a1f7f170451d550c8bc66b2ecb5ccc2d8648
tsiguenz <tsiguenz@student.42.fr>
2023-10-30 16:25:26 +0100

    change c style formating

--- .config/nvim/ftplugin/c.lua
+++ .config/nvim/ftplugin/c.lua
@@ -1,4 +1,4 @@
-vim.bo.tabstop = 2 -- size of a hard tabstop
-vim.bo.shiftwidth = 2 -- size of an indentation
-vim.bo.expandtab = false -- never uses spaces instead of tab characters
-vim.bo.softtabstop = 2 -- number of spaces a <Tab> counts for.
+vim.bo.tabstop = 4 -- size of a hard tabstop
+vim.bo.shiftwidth = 4 -- size of an indentation
+vim.bo.expandtab = true -- always uses spaces instead of tab characters
+vim.bo.softtabstop = 4 -- number of spaces a <Tab> counts for.
