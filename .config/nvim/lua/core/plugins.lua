ed37e216b32ed833e3eeb19008e765268abb0e5b
tsiguenz <tsiguenz@student.42.fr>
2024-01-12 19:21:17 +0100

    change null-ls by none-ls and delete completion plugins

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -45,16 +45,16 @@ return require("packer").startup(function(use)
 		},
 	})
 	-- lint and format
-	use("jose-elias-alvarez/null-ls.nvim")
+	use("nvimtools/none-ls.nvim")
 	use("jay-babu/mason-null-ls.nvim")
 	-- Autocompletion plugin
-	use({
-		"hrsh7th/nvim-cmp",
-		requires = {
-			-- Adds LSP completion capabilities
-			"hrsh7th/cmp-nvim-lsp",
-		},
-	})
+	-- 	use({
+	-- 		"hrsh7th/nvim-cmp",
+	-- 		requires = {
+	-- 			-- Adds LSP completion capabilities
+	-- 			"hrsh7th/cmp-nvim-lsp",
+	-- 		},
+	-- 	})
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
