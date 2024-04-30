76dcdb99d0709f9b569d09e657a158412530e42a
tsiguenz <tsiguenz@student.42.fr>
2024-04-30 22:44:31 +0200

    set tabs instead of space for c

--- .config/nvim/ftplugin/c.lua
+++ .config/nvim/ftplugin/c.lua
@@ -1,4 +1,4 @@
-vim.bo.tabstop = 2      -- size of a hard tabstop
-vim.bo.shiftwidth = 2   -- size of an indentation
-vim.bo.expandtab = true -- always uses spaces instead of tab characters
-vim.bo.softtabstop = 2  -- number of spaces a <Tab> counts for.
+vim.bo.tabstop = 2 -- size of a hard tabstop
+vim.bo.shiftwidth = 2 -- size of an indentation
+vim.bo.expandtab = false -- always uses tab instead of space characters
+vim.bo.softtabstop = 2 -- number of spaces a <Tab> counts for.
